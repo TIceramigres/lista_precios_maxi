f1a2930e3c3ef95a6ce65d4dadb0fe36ffcc8855
wasanabriag37 <wasanabriag37@gmail.com>
2024-05-23 16:22:49 -0500

    cambios para ubuntu

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/administrador/projects/apps/info_diario_max/.venv/bin/activate
-python /home/administrador/projects/apps/info_diario_max/main.py
+source /home/administrador/projects/lista_precios/info_diario_max/lista_precios_max/.venv/bin/activate
+python /home/administrador/projects/lista_precios/info_diario_max/lista_precios_max/main.py
