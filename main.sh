1df2a735ff70a651e0717ae6da98dc0d3fb7ebed
TIceramgres <ares@maxicassa.com>
2024-05-24 08:01:52 -0500

    cambios ubuntu

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/administrador/projects/lista_precios/lista_precios_maxi/.venv/bin/activate
-python /home/administrador/projects/lista_precios/lista_precios_maxi/main.py
+source /home/administrador/projects/lista_precios/lista_precios_maxi/venv/Scripts/activate
+python3 /home/administrador/projects/lista_precios/lista_precios_maxi/main.py
